aba63baf2c029d0db25c168fdddb5b92302be5c9
badalharit <badalharitmrt@gmail.com>
2025-01-19 21:30:31 +0530

    Conditional Rendering

--- myapp/src/screens/RegistrationForm.js
+++ myapp/src/screens/RegistrationForm.js
@@ -1,6 +1,8 @@
 import React from "react";
+import WelcomeMembers from "./WelcomeMembers";
 
 function RegistrationForm() {
+  let age = 17;
     function RegisterTheUser(e){
         e.preventDefault(); // Do not reload the screen on submitting the Form
         alert("Registration Successful!");
@@ -12,6 +14,9 @@ function RegistrationForm() {
             <label>Username: </label>
             <input type="text" name="username" required/>
             <br/>
+            <label>Age: </label>
+            <input type="number" name="age" required/>
+            <br/>
             <label>Password: </label>
             <input type="password" name="password" required/>
             <br/>
@@ -20,6 +25,8 @@ function RegistrationForm() {
             <br/>
             <input type="submit" onClick={RegisterTheUser} value="Register"/>
         </form>
+        {age >= 18? <WelcomeMembers name="Mohan"/>: <WelcomeMembers name='Rohan'></WelcomeMembers>}
+          
       </div>  
     );
 }
