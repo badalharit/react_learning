d5da1ea92712acbb6637c545848002d699bdf95e
badalharit <badalharitmrt@gmail.com>
2025-01-19 14:38:31 +0530

    First commit with `create-react-app` layout

---
+++ myapp/src/LandingPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function LandingPage() {
+    return (
+        <div className="landing-page">
+            <div className="landing-page__header">
+                <h1 className="landing-page__title">Welcome to our website</h1>
+                <p className="landing-page__description">This is a description of our website</p>
+            </div>
+        </div>
+    );
+}
+
+export default LandingPage;
