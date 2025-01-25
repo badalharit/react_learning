9537e3d44840d3fc59d41b14bbb2b474ceab7007
badalharit <badalharitmrt@gmail.com>
2025-01-25 20:57:36 +0530

    Used `useState` Hook for a form

--- myapp/src/screens/RegistrationForm.js
+++ myapp/src/screens/RegistrationForm.js
@@ -6,37 +6,49 @@ function RegistrationForm() {
   /**
    * Enabling Hooks
    */
-      // const [name,setName] = useState();
+      const [name,setName] = useState();
       const [email,setEmail] = useState(); 
-      // const [password,setPassword] = useState();
+      const [password,setPassword] = useState();
+      const [cnfrmPassword,confirmPassword] = useState();
+
   // let age = 17;
     function RegisterTheUser(e){
         e.preventDefault(); // Do not reload the screen on submitting the Form
         alert("Registration Successful!");
     }
-    console.log(email)
+
+    console.log(name+':'+email+':'+password+':'+cnfrmPassword)
     return(
       <div className="form-container">
         <h2>Registration Form</h2>
         <form onSubmit={RegisterTheUser}>
           <div className="form-group">
             <label htmlFor="fullname">Full Name</label>
-            <input type="text" id="fullname" name="fullname" placeholder="Enter your full name" />
+            <input type="text" id="fullname" name="fullname" placeholder="Enter your full name" 
+              value={name} 
+              onChange={(e) => setName(e.target.value)}            
+            />
           </div>
           <div className="form-group">
             <label htmlFor="email">Email</label>
             <input type="email" id="email" name="email" placeholder="Enter your email" 
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)}
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" placeholder="Enter your password" />
+            <input type="password" id="password" name="password" placeholder="Enter your password" 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="confirm_password">Confirm Password</label>
-            <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirm your password" />
+            <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirm your password" 
+              value={cnfrmPassword} 
+              onChange={(e) => confirmPassword(e.target.value)}            
+            />
           </div>
           <input type="submit" className="submit-btn" value="Register" />
         </form>
